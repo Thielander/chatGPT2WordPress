a1af076fbe564fc0c9a2d954d0d1692ce151e574
Alexander Thiele <133631302+Thielander@users.noreply.github.com>
2023-06-09 21:07:13 +0200

    Update chatGPT2WordPress.py

--- chatGPT2WordPress.py
+++ chatGPT2WordPress.py
@@ -1,3 +1,21 @@
+#  code by Alexander Thiele 
+#  https://www.linkedin.com/in/thielander/
+#  alexander@thiele.es
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA#
+#
 import requests
 import sys
 import os
